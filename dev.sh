9df0faadb2e19999c85879c6a74862fc88c6614d
ankarenko <ankarenkosergey@gmail.com>
2023-03-29 14:38:49 -0300

    image size optimization scripts

--- dev.sh
+++ dev.sh
@@ -1,8 +1,9 @@
+docker image build -t jekyll_local . &&
 docker run --rm \
   -v="$PWD:/srv/jekyll" \
   -p 4000:4000 \
   --volume jekyll_gems:/usr/local/bundle \
-  jekyll/jekyll:3.8 \
+  jekyll_local \
   jekyll \
   serve \
   --config _config.yml,_config_development.yml \
