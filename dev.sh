649725a33b59e7a82a3eef4638fa7a15f2b1b51d
ankarenko <ankarenkosergey@gmail.com>
2023-03-30 16:02:54 -0300

    in progress

--- dev.sh
+++ dev.sh
@@ -1,10 +1,8 @@
-sh ./build_images.sh && \
-docker image build -t jekyll_local . && \
 docker run --rm \
   -v="$PWD:/srv/jekyll" \
   -p 4000:4000 \
   --volume jekyll_gems:/usr/local/bundle \
-  jekyll_local \
+  jekyll/jekyll:3.8 \
   jekyll \
   serve \
   --config _config.yml,_config_development.yml \
---
+++ gen_resized.sh
@@ -0,0 +1,40 @@
+OUTPUT_PATH=${PWD}/assets/images
+INPUT_PATH=./test/*
+IMAGE_DIRECTORY=${PWD}/assets/images
+OUTPUT_WS="800 480 320"
+
+rm -rf $IMAGE_DIRECTORY/resized
+rm -rf $IMAGE_DIRECTORY/webp
+
+#magick mogrify -format webP -path ~/Pictures/WebP/ *.png
+
+for d in  $IMAGE_DIRECTORY/*; do
+  [ -L "${d%/}" ] && continue
+  image_folder=$(basename $d)
+
+  echo "Processing $d..."
+  for w in $OUTPUT_WS; do
+    out_path=$OUTPUT_PATH/resized/$image_folder/$w
+    mkdir $out_path -p
+
+    echo "Generating $w..." && \
+    mogrify \
+    -path $out_path \
+    -filter Triangle \
+    -define filter:support=2 \
+    -thumbnail $w \
+    -unsharp 0.25x0.25+8+0.065 \
+    -dither None \
+    -posterize 136 \
+    -quality 82 \
+    -define jpeg:fancy-upsampling=off \
+    -define png:compression-filter=5 \
+    -define png:compression-level=7 \
+    -define png:compression-strategy=1 \
+    -define png:exclude-chunk=all \
+    -interlace none \
+    -colorspace sRGB \
+    -strip $d/*
+  done
+
+done
