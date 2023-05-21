bed31c175acc77940a93552780386769ef318876
ankarenko <ankarenkosergey@gmail.com>
2023-05-20 21:49:24 -0300

    fuego article

--- dev.sh
+++ dev.sh
@@ -1,10 +1,7 @@
-docker run --rm \
+docker run --rm -it \
   -v="$PWD:/srv/jekyll" \
   -p 4000:4000 \
   --volume jekyll_gems:/usr/local/bundle \
   jekyll/jekyll:3.8 \
-  jekyll \
-  serve \
-  --config _config.yml,_config_development.yml \
-  --drafts
+  bash
 
